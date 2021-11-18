c548a58d3b758a7c05b03cae920c23f646105105
sibel <sibel10789@gmail.com>
2021-11-18 16:48:47 +0300

    calc2

--- dist/script.js
+++ dist/script.js
@@ -9,34 +9,85 @@
 /***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
 
 __webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _services_services__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../services/services */ "./src/js/services/services.js");
+
+
 const calc = (state, size, material, options, promocode, result) => {
   const sizeBlock = document.querySelector(size),
         materialBlock = document.querySelector(material),
         optionsBlock = document.querySelector(options),
         promocodeBlock = document.querySelector(promocode),
-        resultBlock = document.querySelector(result);
-  let sum = 0;
+        resultBlock = document.querySelector(result),
+        calcForm = document.querySelector('.calc_form');
+  let sum = 0,
+      priceList = {};
 
   const calcFunc = () => {
     sum = Math.round(+sizeBlock.value * +materialBlock.value + +optionsBlock.value);
 
     if (sizeBlock.value == '' || materialBlock.value == '') {
       resultBlock.textContent = "Пожалуйста, выберите размер и материал картины";
-    } else if (promocodeBlock.value === 'IWANTPOPART') {
+    } else if (priceList.promocode.indexOf(promocodeBlock.value) != -1) {
       resultBlock.textContent = Math.round(sum * 0.7);
       state.result = Math.round(sum * 0.7);
-      console.log(state);
     } else {
       resultBlock.textContent = sum;
       state.result = sum;
-      console.log(state);
     }
   };
 
-  sizeBlock.addEventListener('change', calcFunc);
-  materialBlock.addEventListener('change', calcFunc);
-  optionsBlock.addEventListener('change', calcFunc);
-  promocodeBlock.addEventListener('input', calcFunc);
+  const setValue = (option, block) => {
+    let el = block;
+    el.addEventListener('change', function (e) {
+      let value = e.target.value;
+
+      if (value === 'option value') {
+        location.href = ''; // ссылка для редеректа
+      }
+    });
+    const {
+      size,
+      material,
+      options
+    } = priceList;
+
+    for (let key in size) {
+      console.log(key);
+      console.log(sizeBlock.value);
+
+      if (size.key == sizeBlock.textContent) {
+        console.log(key);
+        console.log(sizeBlock.value);
+      }
+    }
+  };
+
+  const getPrice = () => {
+    let statusMessage = document.createElement('div');
+    statusMessage.classList.add('status');
+    calcForm.appendChild(statusMessage);
+    (0,_services_services__WEBPACK_IMPORTED_MODULE_0__.getResource)('assets/db.json').then(res => {
+      priceList = res;
+      console.log(priceList);
+      calcForm.removeEventListener('mouseover', getPrice);
+      sizeBlock.addEventListener('change', calcFunc);
+      sizeBlock.addEventListener('change', setValue);
+      materialBlock.addEventListener('change', calcFunc);
+      optionsBlock.addEventListener('change', calcFunc);
+      promocodeBlock.addEventListener('input', calcFunc);
+    }).catch(error => {
+      console.log(error);
+      statusMessage.textContent = 'Что-то пошло не так...';
+    }).finally(() => {
+      setTimeout(() => {
+        if (statusMessage) {
+          statusMessage.textContent = '';
+        }
+      }, 5000);
+    });
+  };
+
+  calcForm.addEventListener('mouseover', getPrice);
 };
 
 /* harmony default export */ __webpack_exports__["default"] = (calc);
@@ -418,13 +469,13 @@ const showMoreStyles = (trigger, wrap) => {
   btn.addEventListener('click', function () {
     let statusMessage = document.createElement('div');
     statusMessage.classList.add('status');
-    wrapper.appendChild(statusMessage);
-    (0,_services_services__WEBPACK_IMPORTED_MODULE_0__.getResource)('http://localhost:3000/styles') // getResource('assets/db.json')
-    .then(res => {
-      console.log(res);
-      createCards(res); // console.log(res.styles);
-      // createCards(res.styles);
+    wrapper.appendChild(statusMessage); // getResource('http://localhost:3000/styles')
 
+    (0,_services_services__WEBPACK_IMPORTED_MODULE_0__.getResource)('assets/db.json').then(res => {
+      // console.log(res);
+      // createCards(res);
+      console.log(res.styles);
+      createCards(res.styles);
       btn.remove();
     }).catch(error => {
       console.log(error);
--- src/js/modules/calc.js
+++ src/js/modules/calc.js
@@ -5,16 +5,18 @@ const calc = (state, size, material, options, promocode, result) => {
           materialBlock = document.querySelector(material),
           optionsBlock = document.querySelector(options),
           promocodeBlock = document.querySelector(promocode),
-          resultBlock = document.querySelector(result);
+          resultBlock = document.querySelector(result),
+          calcForm = document.querySelector('.calc_form');
 
-    let sum = 0;
+    let sum = 0,
+        priceList = {};
 
     const calcFunc = () => {
         sum = Math.round((+sizeBlock.value) * (+materialBlock.value) + (+optionsBlock.value));
 
         if (sizeBlock.value == '' || materialBlock.value == '') {
             resultBlock.textContent = "Пожалуйста, выберите размер и материал картины";
-        } else if (promocodeBlock.value === 'IWANTPOPART') {
+        } else if (priceList.promocode.indexOf(promocodeBlock.value) != -1) {
             resultBlock.textContent = Math.round(sum * 0.7);
             state.result = Math.round(sum * 0.7);
         } else {
@@ -22,20 +24,42 @@ const calc = (state, size, material, options, promocode, result) => {
             state.result = sum;
         }
     };
+    const setValue = (option, block) => {
+        let el = block;
+        el.addEventListener('change', function(e){
+            let value = e.target.value;
+            if(value === 'option value'){
+                    location.href = '';  // ссылка для редеректа
+            }
+        });
+        const {size, material, options} = priceList;
+        for (let key in size) {
+            console.log(key);
+            console.log(sizeBlock.value);
+            if(size.key == sizeBlock.textContent){
+                console.log(key);
+                console.log(sizeBlock.value);
+            } 
 
-    const getPrice = (param, options) => {
+        }
+    };
+
+
+    const getPrice = () => {
         let statusMessage = document.createElement('div');
         statusMessage.classList.add('status');
-        resultBlock.parentNode.appendChild(statusMessage);
+        calcForm.appendChild(statusMessage);
 
         getResource('assets/db.json')
             .then(res => {
-                console.log(res.param);
-                res.param.forEach((key, val) => {
-                    if(key == options.textContent){
-                        options.setAttribute('value', val)
-                    }
-                });
+                priceList = res;
+                console.log(priceList);
+                calcForm.removeEventListener('mouseover', getPrice);
+                sizeBlock.addEventListener('change', calcFunc);
+                sizeBlock.addEventListener('change', setValue);
+                materialBlock.addEventListener('change', calcFunc);
+                optionsBlock.addEventListener('change', calcFunc);
+                promocodeBlock.addEventListener('input', calcFunc);
             })
             .catch(error => {
                 console.log(error);
@@ -49,12 +73,10 @@ const calc = (state, size, material, options, promocode, result) => {
                     }
                 }, 5000);
             });
-    });
+    };
 
-    sizeBlock.addEventListener('change', calcFunc);
-    materialBlock.addEventListener('change', calcFunc);
-    optionsBlock.addEventListener('change', calcFunc);
-    promocodeBlock.addEventListener('input', calcFunc);
+    calcForm.addEventListener('mouseover', getPrice);
+
 };
 
 export default calc;
